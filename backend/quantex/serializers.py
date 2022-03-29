c4e017f8da7c98e3d8da00a76df3fba048812d58
nahimr <nahim.rouidjali@gmail.com>
2022-03-29 13:12:24 +0100

    Add the MarketDataSerializer to Instrument's data property

--- backend/quantex/serializers.py
+++ backend/quantex/serializers.py
@@ -2,12 +2,15 @@ from rest_framework import serializers
 from quantex.market.market_data import MarketData
 from quantex.market.instrument import Instrument
 
-class InstrumentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Instrument
-        fields = ('name', 'symbol', 'region', 'data', 'cashFlow', 'details')
-
 class MarketDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = MarketData
         fields = ('symbol', 'date', 'open', 'close', 'low', 'high', 'volume', 'dividends', 'stocks_splits')
+
+
+class InstrumentSerializer(serializers.ModelSerializer):
+    # data = MarketDataSerializer(many=False, read_only=True)
+    
+    class Meta:
+        model = Instrument
+        fields = ('name', 'symbol', 'region', 'data', 'cashFlow', 'details')
