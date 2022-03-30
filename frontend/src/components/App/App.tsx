34c1e72fa19a821cfdb11ace58bf8fd7b75c8d2c
nahimr <nahim.rouidjali@gmail.com>
2022-03-30 19:51:30 +0200

    Restructure folders and  added components

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import logo from '../../assets/logo.svg';
+import './App.css';
+
+
+class App extends Component<any, any> 
+{
+  constructor(props: any) {
+    super(props)
+
+
+  }
+
+  render(): React.ReactNode {
+    return (
+      <Toolbar>
+          
+      </Toolbar>
+    )
+  }
+
+}
+
+export default App;
---
+++ frontend/src/components/Navigation/Toolbar/Toolbar.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class Toolbar extends Component<any, any>
+{
+  constructor(props: any)
+  {
+    super(props);
+
+
+  }
+
+
+}
+
+export default Toolbar;
